f9b5f26871cb6a11365d4e48bde3bc6ceb0de5be
Devin Zimmerman <devinzimmerman@Devins-MacBook-Pro.local>
2024-09-30 22:05:38 -0600

    wip

---
+++ src/layouts/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card mb-5">
+    <Menubar :model="items" />
+  </div>
+  <slot />
+</template>
+
+<style scoped>
+img {
+  width: 50px;
+}
+</style>
+
+<script setup>
+import { ref } from 'vue';
+
+const items = ref([
+  { label: 'Poker Night' }
+])
+</script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,37 @@
 import './assets/main.css';
 
-import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
+
+import Card from 'primevue/card';
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
+import Menubar from 'primevue/menubar'
+import Ripple from 'primevue/ripple';
+
+import HomeLayout from './layouts/Home.vue';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+app.component('Card', Card);
+app.component('Column', Column);
+app.component('DataTable', DataTable);
+app.component('Menubar', Menubar);
+
+app.component('HomeLayout', HomeLayout)
+
+app.directive('ripple', Ripple);
 
 app.mount('#app');
